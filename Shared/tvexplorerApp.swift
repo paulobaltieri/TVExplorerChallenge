7bac7ceadf7f32632a9abeaed44ee4e787ee1450
Paulo Baltieri <baltieri_paulo@hotmail.com>
2022-06-04 08:34:27 -0300

    Initial Commit

---
+++ Shared/tvexplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvexplorerApp.swift
+//  Shared
+//
+//  Created by paulo Baltieri on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct tvexplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
